9a84dd85f375b23a362487ad6bb680fcf489e2e5
Ender Ayalp <ayalp.ender@gmail.com>
2020-08-14 12:54:46 +0200

    removed componentscan

--- src/main/java/com/codingsession/tbd/TbdApplication.java
+++ src/main/java/com/codingsession/tbd/TbdApplication.java
@@ -2,9 +2,8 @@ package com.codingsession.tbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.codingsession.tbd.api.model", "com.codingsession.tbd"})
+
 @SpringBootApplication
 public class TbdApplication {
 
